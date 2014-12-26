708e12d575a55130715c0e69510ac8442723fbe0
Max Terry <mhterry@gmail.com>
2014-12-26 18:48:32 -0500

    Clean up and begin expanding functionality

--- bureau.js
+++ bureau.js
@@ -1,8 +1,8 @@
-Bureau = {}
+var Bureau = {};
 
 Bureau.settings = {
 	editable: true
-}
+};
 
 Bureau.types = {
 	Boolean: {
@@ -20,7 +20,11 @@ Bureau.types = {
 	Location: {
 
 	}
-}
+};
+
+Bureau.headingKeys = [
+	'_id', 'name', 'type', 'editable', 'filter', 'move'
+]
 
 Bureau.headings = [
 	{
@@ -29,10 +33,10 @@ Bureau.headings = [
 		type: String,
 		editable: true,
 		filter: function() {
-			alert("Click")
+			alert("Click");
 		},
 		move: function() {
-			alert("Drag")
+			alert("Drag");
 		}
 	},
 	{
@@ -40,8 +44,12 @@ Bureau.headings = [
 		name: "Age",
 		type: Number,
 		editable: true,
-		filter: function() { },
-		move: function() { }
+		filter: function() {
+			alert("Click");
+		},
+		move: function() {
+			alert("Click");
+		}
 	},
 	{
 		_id: "things",
@@ -55,7 +63,7 @@ Bureau.headings = [
 		// 	}
 		// },
 		value: function(xs) {
-			return xs.length
+			return xs.length;
 		}
 	},
 	{
@@ -63,37 +71,51 @@ Bureau.headings = [
 		name: "Location",
 		type: Location,
 		editable: true,
-		filter: function() { },
-		move: function() { }
+		filter: function() {
+			alert("Click");
+		},
+		move: function() {
+			alert("Drag");
+		}
 	}
-]
+];
 
 Bureau.data = [
 	{
+		// _id: 'edgar',
 		name: "Edgar",
 		age: 77,
 		things: ["filing-cabinet"],
-		origin: "DC",
-		destination: ""
+		location: "DC"
 	},
 	{
+		// _id: 'dale',
 		name: "Dale",
 		age: 55,
 		things: ["microcassette recorder"],
 		location: "WA"
 	},
 	{
+		// _id: 'clarice',
 		name: "Clarice",
 		age: 52,
 		things: ["dresser", "cabinet"],
 		location: "OH"
 	}
-]
+];
+
+Bureau.addHeading = function(props) {
+	var heading = {};
+	for (var key in Bureau.headingKeys) {
+		heading[i] = props[key] || null;
+	}
+	Bureau.data.push(heading);
+}
 
 Bureau.getHeading = function(key) {
 	for (var i in Bureau.headings) {
 		if (Bureau.headings[i]._id === key) {
-			return Bureau.headings[i]
+			return Bureau.headings[i];
 		}
 	}
 }
@@ -102,31 +124,41 @@ Bureau.getRow = function() {
 	// TODO
 }
 
+Bureau.addRow = function() {
+	// TODO
+}
+
 Bureau.render = function(schema, data) {
-	schema = schema || Bureau.headings
-	data = data || Bureau.data
-	var table = document.createElement('table')
-	var ths = document.createElement('tr')
-	var tr = document.createElement('tr')
+
+	schema = schema || Bureau.headings;
+	data = data || Bureau.data;
+
+	var table = document.createElement('table'),
+		ths = document.createElement('tr'),
+		tr = document.createElement('tr');
+
 	for (var i in schema) {
-		var td = document.createElement('th')
-		td.innerText = schema[i].name
-		td.className = schema[i].type.name
+		var th = document.createElement('th');
+		th.innerText = schema[i].name;
+		th.className = schema[i].type.name;
 		// TODO
-		// td.onClick = schema[i].filter
-		// td.onDrag = schema[i].move
-		tr.appendChild(td)
+		// td.addEventListener('click', function() { alert("ok") });
+		// td.addEventListener('dragstart', schema[i].move);
+		tr.appendChild(th);
 	}
-	table.appendChild(tr)
+	table.appendChild(tr);
+
 	for (var i in data) {
-		var tr = document.createElement('tr')
+		var tr = document.createElement('tr');
 		for (var j in data[i]) {
-			var td = document.createElement('td')
-			var h = Bureau.getHeading(j)
-			td.innerText = h && h.value? h.value(data[i][j]) : data[i][j]
-			tr.appendChild(td)
+			var td = document.createElement('td');
+			var h = Bureau.getHeading(j);
+			td.innerText = h && h.value? h.value(data[i][j]) : data[i][j];
+			tr.appendChild(td);
 		}
-		table.appendChild(tr)
+		table.appendChild(tr);
 	}
-	document.body.innerHTML = table.outerHTML
+
+	return table.outerHTML;
+
 }
