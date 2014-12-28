966d585097f374293c99d10c27340ba180c32c0c
Max Terry <mhterry@gmail.com>
2014-12-28 10:54:20 -0500

    Add heading type default functions; only display columns defined in schema

--- bureau.js
+++ bureau.js
@@ -9,47 +9,59 @@ Bureau.types = {
 
 	},
 	Number: {
-
+		sort: function() {
+			console.log("Sort Number");
+		},
+		filter: function() {
+			console.log("Filter Number");
+		},
+		move: function() {
+			console.log("Drag Number");
+		}
 	},
 	String: {
-
+		sort: function() {
+			console.log("Sort String");
+		},
+		filter: function() {
+			console.log("Filter String");
+		},
+		move: function() {
+			console.log("Drag String");
+		}
 	},
 	Array: {
 
 	},
 	Location: {
-
+		sort: function() {
+			console.log("Sort Location");
+		},
+		filter: function() {
+			console.log("Filter Location");
+		},
+		move: function() {
+			console.log("Drag Location");
+		}
 	}
 };
 
-Bureau.headingKeys = [
-	'_id', 'name', 'type', 'editable', 'filter', 'move'
-]
+Bureau.headingProperties = [
+	'_id', 'name', 'type', 'editable', 'sort', 'filter', 'move'
+];
 
 Bureau.headings = [
 	{
 		_id: "name",
 		name: "Name",
 		type: String,
-		editable: true,
-		filter: function() {
-			alert("Click");
-		},
-		move: function() {
-			alert("Drag");
-		}
+		editable: true
 	},
 	{
 		_id: "age",
 		name: "Age",
 		type: Number,
-		editable: true,
-		filter: function() {
-			alert("Click");
-		},
-		move: function() {
-			alert("Click");
-		}
+		editable: true
 	},
 	{
 		_id: "things",
@@ -70,33 +82,27 @@ Bureau.headings = [
 		_id: "location",
 		name: "Location",
 		type: Location,
-		editable: true,
-		filter: function() {
-			alert("Click");
-		},
-		move: function() {
-			alert("Drag");
-		}
+		editable: true
 	}
 ];
 
 Bureau.data = [
 	{
-		// _id: 'edgar',
+		_id: 'edgar',
 		name: "Edgar",
 		age: 77,
 		things: ["filing-cabinet"],
 		location: "DC"
 	},
 	{
-		// _id: 'dale',
+		_id: 'dale',
 		name: "Dale",
 		age: 55,
 		things: ["microcassette recorder"],
 		location: "WA"
 	},
 	{
-		// _id: 'clarice',
+		_id: 'clarice',
 		name: "Clarice",
 		age: 52,
 		things: ["dresser", "cabinet"],
@@ -104,31 +110,53 @@ Bureau.data = [
 	}
 ];
 
+Bureau._headingKeys = [];
+
+Bureau.getHeadingKeys = function() {
+	if (!Bureau._headingKeys.length) {
+		for (var i in Bureau.headings) {
+			Bureau._headingKeys.push(Bureau.headings[i]._id);
+		}
+	}
+	return Bureau._headingKeys;
+}
+
 Bureau.addHeading = function(props) {
 	var heading = {};
-	for (var key in Bureau.headingKeys) {
+	for (var key in Bureau.headingProperties) {
 		heading[i] = props[key] || null;
 	}
 	Bureau.data.push(heading);
+	Bureau._headingKeys.push(props._id);
 }
 
-Bureau.getHeading = function(key) {
-	for (var i in Bureau.headings) {
-		if (Bureau.headings[i]._id === key) {
-			return Bureau.headings[i];
+Bureau.get = function(obj, key) {
+	for (var i in obj) {
+		if (obj[i]._id === key) {
+			return obj[i];
 		}
 	}
 }
 
-Bureau.getRow = function() {
-	// TODO
+Bureau.getHeading = function(key) {
+	return Bureau.get(Bureau.headings, key);
 }
 
-Bureau.addRow = function() {
-	// TODO
+Bureau.getRow = function(key) {
+	return Bureau.get(Bureau.data, key);
+}
+
+Bureau.addRow = function(obj) {
+	Bureau.data.push(obj);
+}
+
+Bureau.typeDefault = function(heading, fn) {
+	return heading[fn] || Bureau.types[heading.type.name][fn];
 }
 
 Bureau.render = function(schema, data) {
+
+	Bureau.getHeadingKeys();
 
 	schema = schema || Bureau.headings;
 	data = data || Bureau.data;
@@ -141,20 +169,23 @@ Bureau.render = function(schema, data) {
 		var th = document.createElement('th');
 		th.innerText = schema[i].name;
 		th.className = schema[i].type.name;
-		th.onclick = schema[i].filter;
+		th.onclick = Bureau.typeDefault(schema[i], 'sort');
+		th.ondblclick = Bureau.typeDefault(schema[i], 'filter');
 		th.draggable = true;
-		// th.ondrag = schema[i].move; // addEventListener('dragstart', schema[i].move);
+		// TODO Drag
 		tr.appendChild(th);
 	}
 	table.appendChild(tr);
 
 	for (var i in data) {
 		var tr = document.createElement('tr');
-		for (var j in data[i]) {
-			var td = document.createElement('td');
-			var h = Bureau.getHeading(j);
-			td.innerText = h && h.value? h.value(data[i][j]) : data[i][j];
-			tr.appendChild(td);
+		for (var id in data[i]) {
+			if (Bureau._headingKeys.indexOf(id) > -1) {
+				var td = document.createElement('td');
+				var h = Bureau.getHeading(id);
+				td.innerText = h && h.value? h.value(data[i][id]) : data[i][id];
+				tr.appendChild(td);
+			}
 		}
 		table.appendChild(tr);
 	}
